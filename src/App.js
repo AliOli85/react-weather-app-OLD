cd87f52e2114b923df12c213757ed920fa3f017c
Matias Ariel Taranto <matiasariel.taranto@gmail.com>
2021-05-24 11:43:53 +0200

    Fixed attribute

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <footer>
         This project was coded by Alicia Garcia and is 
 
-      <a href="https://https://github.com/AliOli85/react-weather-app" target="_blank">
+      <a href="https://https://github.com/AliOli85/react-weather-app" target="_blank" rel="noreferrer">
       open-sourced on GitHub
       </a>
       </footer>
