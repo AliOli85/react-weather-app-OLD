a48a7acb6a97c5855d89b84b929fedb46aa9bc96
Matias Ariel Taranto <matiasariel.taranto@gmail.com>
2021-05-24 11:56:21 +0200

    Fixed issue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by <a href="https://www.instagram.com/alibooh/?hl=es" target="_blank" rel="nonreferrer">Alicia Garcia</a> and is{" "}
+        This project was coded by <a href="https://www.instagram.com/alibooh/?hl=es" target="_blank" rel="noreferrer">Alicia Garcia</a> and is{" "}
 
       <a href="https://https://github.com/AliOli85/react-weather-app" target="_blank" rel="noreferrer">
       open-sourced on GitHub
