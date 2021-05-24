2974fb88e3a48b181294b44a8ebfe75a80d9179b
Matias Ariel Taranto <matiasariel.taranto@gmail.com>
2021-05-24 14:44:45 +0200

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by <a href="https://www.instagram.com/alibooh/?hl=es" target="_blank" rel="noreferrer">Alicia Garcia</a> and is{" "}
@@ -11,6 +12,7 @@ function App() {
       open-sourced on GitHub
       </a>
       </footer>
+      </div>
     </div>
   );
 }
