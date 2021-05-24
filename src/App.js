2a23ef17c5474ee7a119e758bcf16729ca847086
Matias Ariel Taranto <matiasariel.taranto@gmail.com>
2021-05-24 18:29:26 +0200

    Created Weather component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
       <h1>Weather App</h1>
+
+      <Weather />
       <footer>
         This project was coded by <a href="https://www.instagram.com/alibooh/?hl=es" target="_blank" rel="noreferrer">Alicia Garcia</a> and is{" "}
 
